bf91553fcf88336009ee5d19cf40b393ac5b8cfd
László Székely-Tóth <racer01@users.noreply.github.com>
2017-07-04 10:35:35 +0200

    done

---
+++ LoopDetector/LoopDetector/Node.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoopDetector
+{
+    class Node
+    {
+        public int Data;
+        public Node Next { get; set; }
+
+        public Node(int data, Node next = null)
+        {
+            Data = data;
+            Next = next;
+        }
+
+        public bool HasLoop()
+        {
+            Node slow = this;
+            Node fast = this;
+            do
+            {
+                slow = slow.Next;
+                if (fast.Next != null)
+                {
+                    fast = fast.Next.Next;
+                }
+                else
+                {
+                    break;
+                }
+            } while (slow != fast && slow != null && fast != null);
+            return slow == fast;
+        }
+
+        public List<Node> ListNodes()
+        {
+            List<Node> list = new List<Node>();
+            if (!HasLoop())
+            {
+                list.Add(this);
+                while (list.Last().Next != null)
+                {
+                    list.Add(list.Last().Next);
+                }
+            }
+            return list;
+        }
+    }
+}
---
+++ LoopDetector/LoopDetector/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoopDetector
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Node a = new Node(1);
+            Node b = new Node(2);
+            Node c = new Node(3);
+            Node d = new Node(4);
+            Node e = new Node(5);
+            Node f = new Node(6);
+            Node g = new Node(7);
+            Node h = new Node(8);
+            Node i = new Node(9);
+            a.Next = b;
+            b.Next = c;
+            c.Next = d;
+            d.Next = e;
+            e.Next = f;
+            f.Next = g;
+            g.Next = h;
+            h.Next = i;
+            i.Next = a;
+
+            Console.WriteLine(a.ListNodes().Select(node => node.Data).ToArray());
+
+            Console.WriteLine();
+            Console.WriteLine(a.HasLoop());
+            Console.ReadKey();
+        }
+    }
+}
